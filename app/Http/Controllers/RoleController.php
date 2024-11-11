395766234521921d4bb66d8ed7c5381b8c1c124a
Arrahman47 <rahmanyoung39@gmail.com>
2024-11-11 23:07:19 +0700

    Initial commit

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -6,8 +6,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
-use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Permission; 
+use DB;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
     
@@ -18,7 +18,14 @@ class RoleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-   
+    
+    function __construct()
+    {
+         $this->middleware('permission:role-list|role-create|role-edit|role-delete', ['only' => ['index','store']]);
+         $this->middleware('permission:role-create', ['only' => ['create','store']]);
+         $this->middleware('permission:role-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:role-delete', ['only' => ['destroy']]);
+    }
     
     /**
      * Display a listing of the resource.
@@ -28,6 +35,7 @@ class RoleController extends Controller
     public function index(Request $request): View
     {
         $roles = Role::orderBy('id','DESC')->paginate(5);
+        
         return view('roles.index',compact('roles'))
             ->with('i', ($request->input('page', 1) - 1) * 5);
     }
@@ -40,6 +48,7 @@ class RoleController extends Controller
     public function create(): View
     {
         $permission = Permission::get();
+        
         return view('roles.create',compact('permission'));
     }
     
