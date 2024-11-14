d912a55155eab455126cd7b6a36d261756f933ed
daffaakhadi <akhadidaffa13@gmail.com>
2024-11-14 20:25:06 +0700

    dashboard isi masih polos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
     <div class="text-center">
-        <h1>YEAY KAMU BERHASIL LOGIN KE WEB ACU</h1>
+        <h1>Selamat Datang di Laundry Go!</h1>
+        <p>Yeay! Kamu berhasil login ke layanan Laundry kami.</p>
+        <p>Kami siap membantu kamu mencuci pakaian dengan layanan terbaik. Nikmati pengalaman mencuci yang mudah dan cepat!</p>
+        
+        <div class="mt-4">
+            <a href="{{ route('products.index') }}" class="btn btn-outline-primary btn-lg">
+                Mulai Pemesanan
+            </a>
+            <p class="mt-2">Atau, kelola pemesananmu dengan mudah di <strong>Manajemen Pemesanan</strong>.</p>
+        </div>
     </div>
 @endsection
