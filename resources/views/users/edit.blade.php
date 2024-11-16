34f66ff929d997e6bf51c58930e90bb06fcdcd68
daffaakhadi <akhadidaffa13@gmail.com>
2024-11-16 11:02:37 +0700

    ubah tampilan edit user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,74 +1,74 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Edit User</h2>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="d-flex justify-content-between align-items-center mb-4">
+                <h2 class="text-primary fw-bold">Edit User</h2>
+                <a class="btn btn-outline-primary btn-sm" href="{{ route('users.index') }}">
+                    <i class="fa fa-arrow-left"></i> Back
+                </a>
+            </div>
         </div>
-        <div class="pull-right">
-            <a class="btn btn-primary btn-sm mb-2" href="{{ route('users.index') }}"><i class="fa fa-arrow-left"></i> Back</a>
+    </div>
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Whoops!</strong> There were some problems with your input.
+            <ul class="mt-2 mb-0">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
+    <div class="card shadow-sm">
+        <div class="card-body">
+            <form method="POST" action="{{ route('users.update', $user->id) }}">
+                @csrf
+                @method('PUT')
+
+                <div class="mb-3">
+                    <label for="name" class="form-label"><strong>Name:</strong></label>
+                    <input type="text" name="name" id="name" class="form-control" placeholder="Name" value="{{ $user->name }}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="email" class="form-label"><strong>Email:</strong></label>
+                    <input type="email" name="email" id="email" class="form-control" placeholder="Email" value="{{ $user->email }}">
+                </div>
+
+                <div class="mb-3">
+                    <label for="password" class="form-label"><strong>Password:</strong></label>
+                    <input type="password" name="password" id="password" class="form-control" placeholder="Password">
+                </div>
+
+                <div class="mb-3">
+                    <label for="confirm-password" class="form-label"><strong>Confirm Password:</strong></label>
+                    <input type="password" name="confirm-password" id="confirm-password" class="form-control" placeholder="Confirm Password">
+                </div>
+
+                <div class="mb-3">
+                    <label for="roles" class="form-label"><strong>Role:</strong></label>
+                    <select name="roles[]" id="roles" class="form-select" multiple>
+                        @foreach ($roles as $value => $label)
+                            <option value="{{ $value }}" {{ isset($userRole[$value]) ? 'selected' : '' }}>
+                                {{ $label }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+
+                <div class="text-center">
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fa-solid fa-floppy-disk"></i> Submit
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
-
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-      <strong>Whoops!</strong> There were some problems with your input.<br><br>
-      <ul>
-         @foreach ($errors->all() as $error)
-           <li>{{ $error }}</li>
-         @endforeach
-      </ul>
-    </div>
-@endif
-
-<form method="POST" action="{{ route('users.update', $user->id) }}">
-    @csrf
-    @method('PUT')
-
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Name:</strong>
-                <input type="text" name="name" placeholder="Name" class="form-control" value="{{ $user->name }}">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Email:</strong>
-                <input type="email" name="email" placeholder="Email" class="form-control" value="{{ $user->email }}">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Password:</strong>
-                <input type="password" name="password" placeholder="Password" class="form-control">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Confirm Password:</strong>
-                <input type="password" name="confirm-password" placeholder="Confirm Password" class="form-control">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Role:</strong>
-                <select name="roles[]" class="form-control" multiple="multiple">
-                    @foreach ($roles as $value => $label)
-                        <option value="{{ $value }}" {{ isset($userRole[$value]) ? 'selected' : ''}}>
-                            {{ $label }}
-                        </option>
-                     @endforeach
-                </select>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary btn-sm mt-2 mb-3"><i class="fa-solid fa-floppy-disk"></i> Submit</button>
-        </div>
-    </div>
-</form>
-
-
 @endsection
